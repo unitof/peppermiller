a3b8f61f470b2da3f51c82022d84522d8e86c650
Jacob Ford <j@cobford.com>
2019-07-24 11:33:30 -0400

    Logs sold out dates to console
    Empty output may mean dates available—go Go GO!

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Peppermiller
+
+CLI app to check if August 10–13, 2019 are sold out at the Peppermill resort in Reno.
+
+That's all it does.
+
+## Why?
+
+I have 2 comp nights in August and gotta pounce if a room opens up during my planned trip.
---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // log all JS alerts (let you know if date is sold out)
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    await dialog.dismiss()
+  })
+  
+  await page.goto('https://reserve2.peppermillcas.com/cgi-bin/lansaweb?PROCFUN+RN+RESNET+PRN+funcparms+UP(A2560):;;81019;3;02;;;;;;;;;;;;;;;;;;;;?tower=Any')
+  
+  await page.setViewport({ width: 1402, height: 896 })
+  
+  await page.waitForSelector('.viewport > #roomlist > .body-container > p > .btn')
+  await page.click('.viewport > #roomlist > .body-container > p > .btn')
+
+  // terminate when complete
+  // this listener must be added after click event above
+  // might be a race condition
+  page.on('load', async () => {
+    await browser.close()
+  })
+})()
